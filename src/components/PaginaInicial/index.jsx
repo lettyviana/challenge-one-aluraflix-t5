c7d7a9b065af8f41ae2b01dec8f152c2a9249359
lettyviana <letisviana@gmail.com>
2023-09-13 23:07:03 -0300

    Restruturação da página inicial - componente sem videos ou categorias

--- src/components/PaginaInicial/index.jsx
+++ src/components/PaginaInicial/index.jsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
 import styles from "./PaginaInicial.module.css";
 
-export const PaginaInicial = () => {
+export const PaginaInicialZerada = () => {
   return (
     <>
       <div className={styles.paginaInicial}>
-        <h2>Boas-vindas ao Aluraflix!</h2>
-        <p>Ainda não há vídeos ou categorias cadastrados!</p>
-        <p>
-          Para cadastrar um vídeo, primeiro cadastre uma{" "}
-          <Link to={"/nova-categoria"}>categoria</Link>.
-        </p>
+        <div className={styles.containerPaginaInicial}>
+          <h2>Boas-vindas ao Aluraflix!</h2>
+          <p>Ainda não há vídeos ou categorias cadastrados!</p>
+          <p>
+            Para cadastrar um vídeo, primeiro cadastre uma{" "}
+            <Link to={"/nova-categoria"}>categoria</Link>.
+          </p>
+        </div>
       </div>
     </>
   );
--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -4,16 +4,34 @@ import { Cabecalho } from "../../components/Cabecalho";
 import { Rodape } from "../../components/Rodape";
 import { Destaque } from "../../components/Destaque";
 import { CarrosselInicio } from "../../components/CarrosselInicio";
+import { useNovoVideoContext } from "../../context/useNovoVideoContext";
+import { useCategoriaContext } from "../../context/useCategoriaContext";
+import { PaginaInicialZerada } from "../../components/PaginaInicial";
 
 export function Inicio() {
+  const { videos } = useNovoVideoContext();
+  const { categorias } = useCategoriaContext();
   return (
     <>
       <Cabecalho estilos="inicio">
-        <Link to={'/novo-video'}> <BotaoPadrao estilos="novo-video">Novo vídeo</BotaoPadrao></Link>
+        <Link to={"/novo-video"}>
+          {" "}
+          <BotaoPadrao estilos="novo-video">Novo vídeo</BotaoPadrao>
+        </Link>
       </Cabecalho>
-      <Destaque/>
-      <CarrosselInicio/>
-      <Link to={'/novo-video'}> <BotaoPadrao estilos="novo-video-mobile">Novo vídeo</BotaoPadrao></Link>
+      {videos.length === 0 && categorias.length === 0 ? (
+        <PaginaInicialZerada />
+      ) : (
+        <>
+          <Destaque />
+          <CarrosselInicio />
+        </>
+      )}
+
+      <Link to={"/novo-video"}>
+        {" "}
+        <BotaoPadrao estilos="novo-video-mobile">Novo vídeo</BotaoPadrao>
+      </Link>
       <Rodape estilos="inicio" />
     </>
   );
