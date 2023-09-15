fe78f3780fae5a8cbc21638e26dfaf09f11bc279
lettyviana <letisviana@gmail.com>
2023-09-15 18:46:49 -0300

    atualização link rodapé

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -7,7 +7,7 @@ export const Rodape = ({estilos}) => {
     return(
         <footer className={`${styles.rodape} ${styles[estilos]}`}>
             <Link to={'/'}><img className={styles.logotipo} src={logo} /></Link>
-            <p className={styles.descricaoRodape}>&copy; Desenvolvido por <span className={styles.destaque}><Link to={"https://www.linkedin.com/in/leticiaviana-trad-dev/"}>Letícia Viana</Link></span> | 2023</p>
+            <p className={styles.descricaoRodape}>&copy; Desenvolvido por <span className={styles.destaque}><Link to={"https://www.linkedin.com/in/leticiaviana-trad-dev/"} target="_blank" rel="noreferrer noopener">Letícia Viana</Link></span> | 2023</p>
         </footer>
     )
 }
